e8431f182d8d9b5fb008d94c65b001abc09ee98e
TungThanh <tung111104@st.vimaru.edu.vn>
2026-08-18 09:23:51 +0700

    ex04

---
+++ src/main/java/com/ra/session13_ex03/security/UserDetailServiceCustom.java
@@ -0,0 +1,24 @@
+package com.ra.session13_ex03.security;
+
+import com.ra.session13_ex03.model.entity.User;
+import com.ra.session13_ex03.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailServiceCustom implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Không tìm thấy user với username: " + username));
+
+        return UserPrincipal.create(user);
+    }
+}
